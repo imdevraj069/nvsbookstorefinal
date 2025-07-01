0eb0b42e53c7bff9efc902f391b1fd8b9136246c
Devraj <imdev8252@gmail.com>
2025-07-01 22:49:07 +0530

    analytics

---
+++ src/components/GoogleAnalytics.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Script from "next/script";
+
+export default function GoogleAnalytics({ trackingId }) {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`}
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${trackingId}');
+        `}
+      </Script>
+    </>
+  );
+}
